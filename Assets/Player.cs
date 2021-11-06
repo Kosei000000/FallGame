17fb68fcc0291c97bfec32da5f80e5b665044a2a
Kosei000000 <92195034+Kosei000000@users.noreply.github.com>
2021-11-05 19:43:27 -0700

    得点機能の完成

---
+++ Assets/Player.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public Text ScoreText;
+    private int Score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(speed*Time.deltaTime, 0, 0);
+           
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-speed* Time.deltaTime, 0, 0);
+
+        }
+        ScoreText.text = Score.ToString();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag =="Fall")
+        {
+            Score += 100;
+        }
+        
+    }
+}
